d3a221f20ac5ee5aa5e604298b7db9412fcb4cc6
sakib1416 <sakib1416@gmail.com>
2021-12-14 13:01:08 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yfcjm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+app.get("/", (req,res) => {
+    res.send("Hello from the server side for Book Heaven project");
+});
+
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Server started");
+});
